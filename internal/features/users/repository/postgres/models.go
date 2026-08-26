bb5ea010344722b3fcdebe498293d20f1b558d13
BladeRunner322 <tigrcatwot@gmail.com>
2026-08-26 16:25:48 +0300

    Add users feature template

---
+++ internal/features/users/repository/postgres/models.go
@@ -0,0 +1,38 @@
+package users_postgres_repository
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/kupr666/Orange_Team/internal/core/domain"
+)
+
+type UserModel struct {
+	id               uuid.UUID
+	version          int
+	mail             string
+	fullName         string
+	createdAt        time.Time
+	updatedAt        *time.Time
+	userWorkoutScore int
+	sex              *string
+	weightGrams      *int
+	birthDate        *time.Time
+	heightCm         *int
+}
+
+func domainUserFromModel(model UserModel) domain.User {
+	return domain.User{
+		ID:               model.id,
+		Version:          model.version,
+		Email:            model.mail,
+		FullName:         model.fullName,
+		CreatedAt:        model.createdAt,
+		UpdatedAt:        model.updatedAt,
+		UserWorkoutScore: model.userWorkoutScore,
+		Sex:              model.sex,
+		WeightGrams:      model.weightGrams,
+		BirthDate:        model.birthDate,
+		HeightCM:         model.heightCm,
+	}
+}
